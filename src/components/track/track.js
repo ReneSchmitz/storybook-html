e0f2cbcd9d5deceffd17e2aa6658621669831e8f
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-10-12 21:21:46 +0200

    finalase project

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -11,29 +11,34 @@ import playActionSrc from "../../assets/play-action.svg";
 </div>
 */
 
-export function createTrackElement() {
+export function createTrackElement(title, artist) {
+  /* export function createTrackElement() {
   const title = "Thriller";
   const artist = "Michael Jackson";
-
+*/
   const trackElement = document.createElement("div"); // create a div Element <div></div>
   // divElement.innerText = "This is a track"; //<div> This is a track</div>
   trackElement.className = "track";
-
-  const titleElement = document.createElement("h3"); // create h3 Elment
-  titleElement.innerText = title; // <h3>Billie Jean</h3>
-  const artistElement = document.createElement("p"); // create h3 Elment
-  artistElement.innerText = artist; // <h3>Michael jackson</h3>
-  const imgElement = document.createElement("img"); // create h3 Elment
+  const imgElement = document.createElement("img"); // create img Elment
   imgElement.src =
     "https://the-hollywood-gossip-res.cloudinary.com/iu/s--NeYhqfz---/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1364525135/vintage-michael-jackson.jpg";
   imgElement.alt = "Image of" + artist;
   // alternativ   imgElement.alt = 'Image of $(artist)';
   imgElement.className = "track__image";
+  const titleElement = document.createElement("h3"); // create h3 Elment
+  titleElement.innerText = title; // <h3>Billie Jean</h3>
+  const artistElement = document.createElement("p"); // create p Elment
+  artistElement.innerText = artist; // <p>Michael jackson</p>
   const buttonElement = document.createElement("button");
+  buttonElement.className = "button";
   const playActionElement = document.createElement("img");
   playActionElement.src = playActionSrc;
 
   trackElement.append(imgElement, titleElement, artistElement, buttonElement); //<div><h3>Michael Jackson</h3></div>
   buttonElement.append(playActionElement);
+  buttonElement.onclick = function () {
+    alert("click!");
+  };
+
   return trackElement;
 }
--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -2,4 +2,8 @@ import { createTrackElement } from "./track";
 
 export default { title: "Components/Track", parameter: { layout: "centered" } };
 
-export const beatIt = () => createTrackElement();
+export const billieJean = () =>
+  createTrackElement("Billie Jean", "Michael Jackson");
+export const beTheGirl = () => createTrackElement("Be the girl", "Bebe Rexa");
+export const countryman = () => createTrackElement("Countryman", "Daughtry");
+/*export const beatIt = () => createTrackElement();*/
