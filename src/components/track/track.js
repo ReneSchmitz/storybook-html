333c8ae2f11fad8a61c5e5408fa006ffdf511776
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-10-12 13:53:43 +0200

    Add paragraph and h3

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,10 +1,23 @@
+/*
+<div>
+<img src="--/micheal.png"/>
+<h3>Billie Jean<h3>
+<>Michael Jackson</>
+<button><img src="../play.svg" />
+</button>
+</div>
+*/
+
 export function createTrackElement() {
   const divElement = document.createElement("div"); // create a div Element <div></div>
-  divElement.innerText = "This is a track"; //<div> This is a track</div>
+  // divElement.innerText = "This is a track"; //<div> This is a track</div>
 
   const h3Element = document.createElement("h3"); // create h3 Elment
-  h3Element.innerText = "Michale Jackson"; // <h3>Michael jackson</h3>
+  h3Element.innerText = "Billie Jean"; // <h3>Michael jackson</h3>
+  const pElement = document.createElement("p"); // create h3 Elment
+  pElement.innerText = "Michael Jackson"; // <h3>Michael jackson</h3>
 
   divElement.append(h3Element); //<div><h3>Michael Jackson</h3></div>
+  divElement.append(pElement); //<div><h3>Michael Jackson</h3></div>
   return divElement;
 }
