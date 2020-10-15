50a0e594c9148c2f5bfefc87cfb2b77f6280e13b
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-10-15 23:06:13 +0200

    Implement more divs and define more classes

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,24 +1,46 @@
-import { createElement } from "../../utils/elements";
-import "./dashboard.css";
+import 
 
-export const creatDashboardElement = (dashboard) => {
+import "./dashboard.css";
+import { createElement } from "../../utils/elements";
+
+export const createDashboardElement = (dashboard) => {
   const dashboardElement = new Audio(dashboard.audioSrc);
+  // const iconAltText = `Play ${track.title} by ${track.artist}`;
+  // const playIcon = creatElement("img", {
+  // src: playIconPlay,
+  // alt: iconAltText,
+  // });
   const dashboardActionElement = createElement("div", {
     className: "dashboard_element",
     children: [
       createElement("div", {
         className: "primary__control",
         children: [
-          createElement("img", {
-            src: dashboard.rewingImgSrc,
-            alt: "rewind",
-          }),
+          createElement("div", {
+            className: "player",
+            children: [
+              createElement("div", {
+                className: "player__main",
+                children:[
+                  createElement("button", {
+                    className: "play__button",
+                    onclick: () => {
+                      console.log("Play: Last Song");
+                    },
+                    children: [
+                      createElement("img", {
+                        src: rewingImgSrc,
+                        alt: "Last Song",
+                      }),
+                    ],
+                  }),
           createElement("img", {
             src: dashboard.playImgSrc,
             alt: "play",
           }),
           createElement("img", {
             src: dashboard.forwardImgSrc,
+            alt: "Next Song",
           }),
         ],
       }),
